5f88aa64d2f7391c3315cd91d2d10533578ad385
notxenogenesis <notxenogenisis@gmail.com>
2025-12-14 21:51:57 -0500

    basic app init

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-// TODO: implement main.ts
+import { Application, Assets, Container, Sprite } from 'pixi.js';
+
+(async () => {
+  // Create a new application
+  const app = new Application();
+
+  // Initialize the application
+  await app.init({ background: '#00000000', width: 800, height: 600});
+
+  // Append the application canvas to the document body
+  document.getElementById("app")!.appendChild(app.canvas);
+})();
